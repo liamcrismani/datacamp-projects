84f72d809c624381fdf281006c6b1e95c6df17bf
liamcrismani <liamcrismani@protonmail.com>
2024-06-16 13:49:44 +0100

    update test function

--- calorie-calculator/test_script.py
+++ calorie-calculator/test_script.py
@@ -1,13 +1,15 @@
+import pytest
 from script import nutritional_summary
 
-def test_nutritional_summary(food_dict: dict) -> None:
-    """
-      Summary of nutritional info from all foods in food_dict.
-        E.g {'calories': 733.5, 'total_fat': 32.0, 'protein': 15.55,
-        'carbohydrate': 96.5, 'sugars': 38.025}
-    """
+@pytest.fixture
+def prepare_data():
+    data = {"Croissants, cheese": 150, "Orange juice, raw": 250}
+    return data
+
+def test_nutritional_summary(prepare_data):
     goal = {'calories': 733.5, 'total_fat': 32.0, 'protein': 15.55,
         'carbohydrate': 96.5, 'sugars': 38.025}
-    nutritional_summary({"Croissants, cheese": 150, "Orange juice, raw": 250}) 
+    nutritional_summary(prepare_data)
+    global result
     assert result.keys() == goal.keys()
     assert result.values() == goal.values()
