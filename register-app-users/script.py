9991d48bed301e70f9ca29c4e947cb71ece8e682
liamcrismani <liamcrismani@protonmail.com>
2024-06-30 16:08:28 +0100

    initial commit

---
+++ register-app-users/script.py
@@ -0,0 +1,21 @@
+def validate_user(name: str, email: str, password: str) -> None:
+    if validate_name(name):
+        pass
+    else:
+        raise ValueError("Name must be greater than two characters and contain letters only.")
+    if validate_email(email):
+        pass
+    else:
+        raise ValueError("Invalid email format.")
+    if validate_password(password):
+        pass
+    else:
+        raise ValueError("Invalid password format.")
+    return True
+
+def register_user(name: str, email: str, password: str) -> None:
+    if validate_user(name, email, password):
+        user = {'name': name, 'email': email, 'password': password}
+        return user 
+    else:
+        return False
