ce6bb3120e0d9cf20644cf33626c19b6b995993f
liamcrismani <liamcrismani@protonmail.com>
2024-07-20 16:57:59 +0100

    update docstrings

--- code-review/script.py
+++ code-review/script.py
@@ -1,10 +1,12 @@
 import os
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 def prepare_smartphone_data(file_path):
     """
-    Prepare smartphone data for visualization:
+    Prepare smartphone data for visualization.
         - read csv to DataFrame
         - reducing the number of columns to only those needed for analysis
         - removing records without a battery_capacity value
@@ -13,13 +15,11 @@ def prepare_smartphone_data(file_path):
     :param file_path: the file path where the raw smartphone data is stored
     :return: a cleaned dataset having had the operations above applied to it
     """
-    # Load data
     if os.path.exists(file_path):
         rawData = pd.read_csv(file_path)
     else:
         raise Exception(f"File containing data not found at path {file_path}")
 
-    # Define columns to keep
     columns_to_keep = [
         "brand_name",
         "os",
@@ -27,7 +27,8 @@ def prepare_smartphone_data(file_path):
         "avg_rating",
         "processor_speed",
         "battery_capacity",
-        "screen_size"]
+        "screen_size"
+        ]
 
     trimmedData = rawData.loc[:, columns_to_keep]
 
@@ -40,5 +41,45 @@ def prepare_smartphone_data(file_path):
     return reducedData
 
 
+def column_to_label(column_name):
+    """
+    Convert a column name in a pandas DataFrame to a string that can be
+    used as a label in a plot.
+    
+    :param column_name: string containing original column name
+    :return: string that is ready to be presented on a plot
+    """
+    # Validate that column_name is a string
+    if isinstance(column_name, str):
+        return " ".join(column_name.split("_")).title()
+    
+    # If the value provided is not a string, raise an Exception
+    else:
+        raise Exception("Please makes sure to pass a value of type 'str'.")
+
+
+def visualize_versus_price(clean_data, x):
+    """
+    Use seaborn and matplotlib to identify a pattern between avg_rating and 
+    battery_capacity.
+    
+    :param clean_data: a pandas DataFrame containing cleaned smartphone data
+    :param x: variable to be plotted on the x-axis
+    :return: None
+    """
+    # Create the scatterplot
+    sns.scatterplot(x=x, y="price", data=clean_data, hue="os")
+    
+    # Add x and y labels
+    plt.xlabel(column_to_label(x))
+    plt.ylabel("Price ($)")
+    
+    # Add a title to the plot
+    plt.title(f"{column_to_label(x)} vs. Price")
+    
+    
 # Call the function
 cleaned_data = prepare_smartphone_data("./data/smartphones.csv")
+
+# Call the visualize_versus_price function
+visualize_versus_price(cleaned_data, "processor_speed")
