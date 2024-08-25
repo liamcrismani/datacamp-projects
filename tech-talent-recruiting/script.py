2b09a57595f18be06f9fca0f7926f23edccf3f12
liamcrismani <liamcrismani@protonmail.com>
2024-08-25 18:44:43 +0100

    reformat with pycodestyle/pydocstyle

--- tech-talent-recruiting/script.py
+++ tech-talent-recruiting/script.py
@@ -6,7 +6,7 @@ from typing import Match
 resumes: pd.DataFrame = pd.read_csv('resumes.csv')
 
 # Define regex
-jobs_regex = r"^([A-Z\s\.\,\-]+)\b" 
+jobs_regex = r"^([A-Z\s\.\,\-]+)\b"
 skills_regex = r"\b(python|sql|r|excel)\b"
 edu_regex = r"\b(PhD|Master|Bachelor)\b"
 
@@ -15,40 +15,39 @@ job_titles: list[str] = []
 tech_skills: list[str] = []
 education: list[str] = []
 
+
 # functions to extract data
 def get_job_title(resume: str) -> Match[str] | None:
     """Extract job titles from a string column."""
-
     return re.search(jobs_regex, resume)
-    
+
 
 def get_skills(resume: str) -> list[str]:
     """Get a list of skills from a string."""
-
     return re.findall(skills_regex, resume, flags=re.IGNORECASE)
 
 
 def get_education(resume: str) -> list[str]:
     """Get a list of education history."""
-    
     return re.findall(edu_regex, resume, flags=re.IGNORECASE)
+
 
 # Extract data and add to lists
 for resume in resumes['Resume_str']:
 
     # extract job titles
     jobs_match = get_job_title(resume)
-    
+
     # if a job title is found, add to the list
     if jobs_match is not None:
         job_titles.append(jobs_match.group(0).strip())
     else:
         job_titles.append("")
-   
+
     # extract tech skills
     skills = set([skill.title() for skill in get_skills(resume)])
     tech_skills.append(", ".join(skills))
-    
+
     # extract education history
     education_history = set([education.title() for education in get_education(resume)])
     education.append(", ".join(education_history))
